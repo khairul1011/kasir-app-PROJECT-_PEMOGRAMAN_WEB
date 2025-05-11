5b91281d8f42054a571fddf07215387f6b66aeca
KHAIRUL ANUAR <erul0575@gmail.com>
2025-05-11 14:58:29 +0700

    update header

--- layouts/header.php
+++ layouts/header.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <title>Dashboard - Aplikasi Kasir</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <!-- Tailwind CSS (wajib dulu) -->
-    <script src="https://cdn.tailwindcss.com"></script>
-
     <!-- Flowbite (JS buat interaktivitas) -->
     <script src="https://unpkg.com/flowbite@2.3.0/dist/flowbite.min.js"></script>
+    <link href="https://unpkg.com/flowbite@latest/dist/flowbite.min.css" rel="stylesheet" />
 
 </head>
+
 <body class="bg-gray-100 text-gray-800">
-
-
-
